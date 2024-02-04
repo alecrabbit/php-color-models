c10710dbcad76714c191f7d4efbb988ac6f00e63
Oleksii <11045453+alecrabbit@users.noreply.github.com>
2024-02-04 21:47:49 +0200

    ~

---
+++ src/Color/Model/Converter/Core/XYZToLAB.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlecRabbit\Color\Model\Converter\Core;
+
+use AlecRabbit\Color\Model\Contract\Converter\Core\IIlluminant;
+use AlecRabbit\Color\Model\Contract\DTO\DColor;
+use AlecRabbit\Color\Model\Converter\Core\A\ACoreConverter;
+use AlecRabbit\Color\Model\Converter\Core\Illuminant\D65;
+use AlecRabbit\Color\Model\DTO\DLAB as LAB;
+use AlecRabbit\Color\Model\DTO\DXYZ as XYZ;
+
+/** @internal */
+final readonly class XYZToLAB extends ACoreConverter
+{
+    private const DELTA = 6.0 / 29.0;
+    private const COEFFICIENT = 4.0 / 29.0;
+
+    public function __construct(
+        private IIlluminant $illuminant = new D65(),
+        int $precision = self::CALC_PRECISION
+    ) {
+        parent::__construct(XYZ::class, $precision);
+    }
+
+    protected function doConvert(DColor $color): DColor
+    {
+        /** @var XYZ $color */
+        // Normalize XYZ values
+        $x = $color->x / $this->illuminant->x;
+        $y = $color->y / $this->illuminant->y;
+        $z = $color->z / $this->illuminant->z;
+
+        // Calculate LAB values
+        $x = ($x > 0.008856) ? $x ** (1 / 3) : (7.787 * $x + 16 / 116);
+        $y = ($y > 0.008856) ? $y ** (1 / 3) : (7.787 * $y + 16 / 116);
+        $z = ($z > 0.008856) ? $z ** (1 / 3) : (7.787 * $z + 16 / 116);
+
+        $l = max(0, 116 * $y - 16);
+        $a = 10 * ($x - $y);
+        $b = 4 * ($y - $z);
+
+        return new LAB(
+            round($l, $this->precision),
+            round($a, $this->precision),
+            round($b, $this->precision),
+            round($color->alpha, $this->precision)
+        );
+    }
+
+    protected function correction(float $v): float
+    {
+        return $v > self::DELTA
+            ? $v ** 3.0
+            : 3.0 * ($v - self::COEFFICIENT) * self::DELTA * self::DELTA;
+    }
+
+}
---
+++ tests/Color/Unit/Model/Converter/Core/XYZToLABTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlecRabbit\Tests\Color\Unit\Model\Converter\Core;
+
+
+use AlecRabbit\Color\Model\Contract\Converter\Core\ICoreConverter;
+use AlecRabbit\Color\Model\Converter\Core\XYZToLAB;
+use AlecRabbit\Color\Model\DTO\DLAB;
+use AlecRabbit\Color\Model\DTO\DXYZ;
+use AlecRabbit\Color\Model\Exception\InvalidArgument;
+use AlecRabbit\Tests\TestCase\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+
+final class XYZToLABTest extends TestCase
+{
+    public static function canConvertXYZToLABDataProvider(): iterable
+    {
+        yield from [
+            // [expected, incoming]
+            [new DLAB(0, 0, 0), new DXYZ(0, 0, 0)],
+            [new DLAB(0.749012, 0.8, 0.639216), new DXYZ(0.5, 0.5, 0.5)],
+//            [new DLAB(0.749012, -0.8, 0.639216), new DXYZ(0.331111, 0.48121, 0.264334)],
+//            [new DLAB(0.233, -0.8, -0.639216), new DXYZ(0.016474, 0.038887, 0.134961)],
+//            [new DLAB(0.729412, 0.368627, 0.270588), new DXYZ(0.253287, 0.188771, 0.079388)],
+//            [new DLAB(0.290196, 0.968627, 0.8), new DXYZ(0.469782, 0.723315, 0.686005)],
+//            [new DLAB(0.54902, 0.372549, 0.211765), new DXYZ(0.155743, 0.140275, 0.053766)],
+//            [new DLAB(0.329412, 0.443137, 1), new DXYZ(0.276051, 0.209124, 0.971701)],
+//            [new DLAB(0.709804, 0.368627, 0.262745), new DXYZ(0.240739, 0.182371, 0.075614)],
+//            [new DLAB(0.294118, 0.403922, 0.588235), new DXYZ(0.132551, 0.133975, 0.307357)],
+//            [new DLAB(0.960784, 1, 0.027451), new DXYZ(0.734573, 0.909497, 0.138865)],
+        ];
+    }
+
+    #[Test]
+    public function canBeInstantiated(): void
+    {
+        $converter = $this->getTesteeInstance();
+
+        self::assertInstanceOf(XYZToLAB::class, $converter);
+    }
+
+    protected function getTesteeInstance(
+        ?int $precision = null,
+    ): ICoreConverter {
+        return new XYZToLAB(
+            precision: $precision ?? ICoreConverter::CALC_PRECISION,
+        );
+    }
+
+    #[Test]
+    #[DataProvider('canConvertXYZToLABDataProvider')]
+    public function canConvertXYZToLAB(DLAB $expected, DXYZ $incoming): void
+    {
+        $converter = $this->getTesteeInstance();
+
+        self::assertEquals($expected, $converter->convert($incoming));
+    }
+
+    #[Test]
+    public function throwsIfModelIsNotCorrect(): void
+    {
+        $input = new DLAB(0, 0, 0);
+
+        $this->expectException(InvalidArgument::class);
+        $this->expectExceptionMessage(
+            'Color must be instance of "AlecRabbit\Color\Model\DTO\DXYZ", "AlecRabbit\Color\Model\DTO\DLAB" given.'
+        );
+
+        $testee = $this->getTesteeInstance();
+
+        $testee->convert($input);
+    }
+}
